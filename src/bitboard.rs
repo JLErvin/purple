e88af232e72d7ff4e3b4f9510e2189479ad2cfbe
Joshua Ervin <ervin.josh17@gmail.com>
2021-02-25 00:24:11 -0800

    Reformat with rustfmt

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -28,14 +28,25 @@ impl Board {
             b_knights: 4755801206503243776,
             b_bishops: 2594073385365405696,
             b_queen: 1152921504606846976,
-            b_king: 576460752303423488
+            b_king: 576460752303423488,
         }
     }
 
-    pub fn pieces(&self) -> Vec<u64>  {
-        vec![self.w_pawns, self.w_rooks, self.w_knights, self.w_bishops, self.w_king, self.w_queen,
-             self.b_pawns, self.b_rooks, self.b_knights, self.b_bishops, self.b_king, self.b_queen]
-
+    pub fn pieces(&self) -> Vec<u64> {
+        vec![
+            self.w_pawns,
+            self.w_rooks,
+            self.w_knights,
+            self.w_bishops,
+            self.w_king,
+            self.w_queen,
+            self.b_pawns,
+            self.b_rooks,
+            self.b_knights,
+            self.b_bishops,
+            self.b_king,
+            self.b_queen,
+        ]
     }
 
     pub fn debug_print(&self) {
@@ -44,7 +55,7 @@ impl Board {
             p |= b
         }
         for i in 1..65 {
-            print!("{}", Board::bit_at_i(p, i-1) as u64 );
+            print!("{}", Board::bit_at_i(p, i - 1) as u64);
             if i % 8 == 0 {
                 println!("");
             }
