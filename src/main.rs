f4d3565f73b7308aa9ef5cb7f421c1732a37f8e6
Joshua Ervin <ervin.josh17@gmail.com>
2021-04-03 11:06:13 -0700

    Refactor magic number generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::board_state::board::BoardState;
-use crate::magic::magicgen::gen_magic_number;
+use crate::components::piece::PieceType;
+use crate::magic::magicgen::MagicGenerator;
 use board_state::fen::*;
 use move_gen::movegen::gen_all_pseduo_legal_moves;
 
@@ -12,5 +13,8 @@ fn main() {
     println!("Hello, world!");
     let b =
         parse_fen(&"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2".to_string());
+    let mut m = MagicGenerator::new(0, PieceType::Rook);
+    let n = m.find_magic_number();
+    println!("{}", n);
     println!();
 }
