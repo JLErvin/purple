cd2d1018edf2d51238fafae4543b3d1e8905ecea
Joshua Ervin <josh.ervin@frontapp.com>
2023-07-17 21:58:29 -0700

    clippy fix

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+
 
 use crate::bitboard::{
     AddPiece, Bitboard, ClearBit, GetBit, New, Shift, INIT_W_BISHOPS, INIT_W_KING, INIT_W_KNIGHTS,
@@ -9,7 +9,7 @@ use crate::piece::PieceType::Rook;
 use crate::piece::{Color, Piece, PieceType, COLOR_COUNT, PIECE_COUNT};
 use crate::square::Square;
 use crate::square::SquareIndex::{A1, A8, C1, C8, D1, D8, E1, E8, F1, F8, G1, G8, H1, H8};
-use crate::table::{ZobristHash, ZobristTable};
+
 
 #[derive(Copy, Clone)]
 pub struct BoardState {
